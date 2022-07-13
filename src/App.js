43d12b45f5d2b6f012d625ca7eb2be60bd4fbe52
Francis Guido <fjguido97@gmail.com>
2022-07-13 13:56:37 -0400

    slider not working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import hubbleCorina from './img/hubble-carina.jpeg';
+import jwtsCorina from './img/jwts-carina.jpg';
 
-function App() {
+import hubbleQuintet from './img/hubble-quintet.png';
+import jwtsQuintet from './img/jwts-quintet.jpg';
+
+import hubbleSmacs from './img/hubble-smacs.webp';
+import jwtsSmacs from './img/jwts-smacs.jpg';
+
+import hubbleSouthernRing from './img/hubble-southern-ring.jpeg';
+import jwtsSouthernRing from './img/jwts-southern-ring.png';
+
+import Slider from './components';
+
+const slider1 = {
+  delay: 2000,
+  original: {
+      url: hubbleCorina,
+      title: 'Hubble'
+  },
+  modified: {
+      url: jwtsCorina,
+      title: 'JWTS'
+  }
+};
+
+const slider2 = {
+  original: {
+      url: hubbleQuintet,
+      title: 'Hubble'
+  },
+  modified: {
+      url: jwtsQuintet,
+      title: 'JWTS'
+  }
+};
+
+const slider3 = {
+  original: {
+      url: hubbleSmacs,
+      title: 'Hubble'
+  },
+  modified: {
+      url: jwtsSmacs,
+      title: 'JWTS'
+  }
+};
+
+const slider4 = {
+  original: {
+      url: hubbleSouthernRing,
+      title: 'Hubble'
+  },
+  modified: {
+      url: jwtsSouthernRing,
+      title: 'JWTS'
+  }
+};
+
+
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          <h2>Slider #1</h2>
+          <Slider {...slider1}/>
+
+          <br/>
+
+          <h2>Slider #2</h2>
+          <Slider {...slider2}/>
+
+          <br/>
+
+          <h2>Slider #3</h2>
+          <Slider {...slider3}/>
+
+          <br/>
+
+          <h2>Slider #4</h2>
+          <Slider {...slider4}/>
+      </div>
+  )
 }
 
 export default App;
